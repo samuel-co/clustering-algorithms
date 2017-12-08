5dca072561623e740ff1fcb418557e6534ca7787
Samuel <samfcongdon@gmail.com>
2017-12-08 14:12:40 -0700

    Increased efficiency of the evaluation metrics

--- Driver.py
+++ Driver.py
@@ -26,32 +26,31 @@ def import_data(file_name):
 def intra_distance(clusters):
     ''' Calculate the average intra-cluster distance for every cluster contained in clusters. Calculated by
         averaging the squared Euclidean distance between every point within each cluster. Want to minimize.'''
-
     total_sum = 0
     for cluster in clusters:
-        cluster_sum = 0
-        for x in cluster:
-            for y in cluster:
-                if x != y:
-                    cluster_sum += math.sqrt(sum([(a - b) ** 2 for a, b in zip(x, y)])) ** 2
-        total_sum += cluster_sum / len(cluster) ** 2
+        cluster_sum = []
+        for i in range(len(cluster)-1):
+            for j in range(i+1, len(cluster)):
+                cluster_sum += [math.sqrt(sum([(a - b) ** 2 for a, b in zip(cluster[i], cluster[j])])) ** 2]
+        if len(cluster_sum) > 0: total_sum += sum(cluster_sum) / len(cluster_sum)
     return total_sum / len(clusters)
+
 
 def inter_distance(clusters):
     ''' Calculate the average inter-cluster distance for each pair of clusters in clusters. Calculated as
-        the average squared Euclidean distance between each point of each pair of clusters. Want to maximize. '''
+           the average squared Euclidean distance between each point of each pair of clusters. Want to maximize. '''
+    if len(clusters) == 1:
+        return 0
 
-    total_sum = 0
-    for c1 in clusters:
-        for c2 in clusters:
-            inner_sum = 0
-            if c1 != c2:
-                for x in c1:
-                    for y in c2:
-                        inner_sum += math.sqrt(sum([(a - b) ** 2 for a, b in zip(x, y)])) ** 2
-                total_sum += inner_sum / (len(c1) * len(c2))
-    return total_sum / len(clusters) ** 2
-
+    total_sum = []
+    for c1 in range(len(clusters)-1):
+        for c2 in range(c1+1, len(clusters)):
+            inner_sum = []
+            for x in clusters[c1]:
+                for y in clusters[c2]:
+                    inner_sum += [math.sqrt(sum([(a - b) ** 2 for a, b in zip(x, y)])) ** 2]
+            total_sum += [sum(inner_sum) / len(inner_sum)]
+    return sum(total_sum) / len(total_sum)
 
 def SSE(clusters):
     ''' Calculate the Sum Square Error (SSE) of the clusters. Calculated as the sum of each points
@@ -59,8 +58,6 @@ def SSE(clusters):
 
     total_sum = 0
     for cluster in clusters:
-        inner_sum = 0
-
         # calulate the mean value of each feature over all the points in the cluster, used to create the mean_point
         feature_sums = []
         for i in range(len(cluster[0])):
@@ -69,10 +66,25 @@ def SSE(clusters):
 
         # sum the SSE of each point in the cluster
         for point in cluster:
-            inner_sum += math.sqrt(sum([(a - b) ** 2 for a, b in zip(point, mean_point)])) ** 2
-        total_sum += inner_sum
+            total_sum += math.sqrt(sum([(a - b) ** 2 for a, b in zip(point, mean_point)])) ** 2
 
     return total_sum
+
+def test_clustering(clusters, time, normal = False):
+    ''' Print the evaluation metrics for a series of metrics. If normal = False, input clusters
+        will be normalize before being evaluated. '''
+
+    if not normal:
+        extremes = [CompLearn.get_min_max_values(cluster) for cluster in clusters]
+        mini, temp = CompLearn.get_min_max_values([pair[0] for pair in extremes])
+        temp, maxi = CompLearn.get_min_max_values([pair[1] for pair in extremes])
+        clusters = [CompLearn.normalize_data(cluster, mini, maxi) for cluster in clusters]
+
+    print("- Number of clusters = {}".format(len(clusters)))
+    print("- Intra-distance = {}".format(intra_distance(clusters)))
+    print("- Inter-distance = {}".format(inter_distance(clusters)))
+    print("- SSE = {}".format(SSE(clusters)))
+    print("- Time elapsed = {}\n".format(time))
 
 
 def main():
@@ -82,35 +94,23 @@ def main():
     start = time.time()
     clusters = KMeans.kmeans_clustering(data, 3, 10000)
     end = time.time()
-    mini, maxi = CompLearn.get_min_max_values(data)
-    normal_clusters = [CompLearn.normalize_data(cluster, mini, maxi) for cluster in clusters]
-    print("- Number of clusters = {}".format(len(clusters)))
-    print("- Intra-distance = {}".format(intra_distance(normal_clusters)))
-    print("- Inter-distance = {}".format(inter_distance(normal_clusters)))
-    print("- SSE = {}".format(SSE(normal_clusters)))
-    print("- Time elapsed = {}\n".format(end - start))
+    test_clustering(clusters, end-start)
+
 
     print("Using DBScan to cluster dataset {}:".format(name))
     start = time.time()
     clusters = DBScan.db_clustering(data, 30, 2.6)
     end = time.time()
-    mini, maxi = CompLearn.get_min_max_values(data)
-    normal_clusters = [CompLearn.normalize_data(cluster, mini, maxi) for cluster in clusters]
-    print("- Number of clusters = {}".format(len(clusters)))
-    print("- Intra-distance = {}".format(intra_distance(normal_clusters)))
-    print("- Inter-distance = {}".format(inter_distance(normal_clusters)))
-    print("- SSE = {}".format(SSE(normal_clusters)))
-    print("- Time elapsed = {}\n".format(end - start))
+    end = time.time()
+    test_clustering(clusters, end - start)
+
 
     print("Using CompLearn to cluster dataset {}:".format(name))
     start = time.time()
-    normal_clusters = CompLearn.complearn_clustering(data, 6, 100000)
+    clusters = CompLearn.complearn_clustering(data, 6, 100000)
     end = time.time()
-    print("- Number of clusters = {}".format(len(normal_clusters)))
-    print("- Intra-distance = {}".format(intra_distance(normal_clusters)))
-    print("- Inter-distance = {}".format(inter_distance(normal_clusters)))
-    print("- SSE = {}".format(SSE(normal_clusters)))
-    print("- Time elapsed = {}\n".format(end-start))
+    end = time.time()
+    test_clustering(clusters, end - start, True)
 
 
 if __name__ == '__main__':
